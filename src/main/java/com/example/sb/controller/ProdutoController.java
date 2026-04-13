796ea88f36b48a6e5fc77ae11c03ba81347f0bb2
Vinícius Rafael Gonçalves <viniciusgoncalves351@gmail.com>
2026-04-13 17:04:19 -0300

    Correct HttpStatus

--- src/main/java/com/example/sb/controller/ProdutoController.java
+++ src/main/java/com/example/sb/controller/ProdutoController.java
@@ -1,10 +1,11 @@
 package com.example.sb.controller;
 
 import java.io.Serializable;
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.sb.entity.Produto;
 import com.example.sb.service.ProdutoService;
@@ -27,27 +29,34 @@ public class ProdutoController implements Serializable{
 	private ProdutoService service;
 	
 	@GetMapping("/produtos")
-	public List<Produto> listarProdutos(){
-		return service.listarProdutos();
+	public ResponseEntity<List<Produto>> listarProdutos(){
+		List<Produto> list = service.listarProdutos();
+		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping("/produtos/{id}")
-	public Optional<Produto> acharPorId(@PathVariable Long id){
-		return service.acharPorId(id);
+	public ResponseEntity<Produto> acharPorId(@PathVariable Long id){
+		Produto obj = service.acharPorId(id);
+		return ResponseEntity.ok().body(obj);
+		
 	}
 	
 	@PostMapping("/produtos")
-	public Produto adicionarProduto(@RequestBody Produto obj) {
-		return service.adicionarProduto(obj);
+	public ResponseEntity<Produto> adicionarProduto(@RequestBody Produto obj) {
+		 obj = service.adicionarProduto(obj);
+		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		 return ResponseEntity.created(uri).body(obj);
 	}
 	
 	@DeleteMapping("/produtos/{id}")
-	public void deleteById(@PathVariable Long id) {
+	public ResponseEntity<Void> deleteById(@PathVariable Long id) {
 		service.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 	@PutMapping("/produtos/{id}")
-	public Produto updateProduto(@PathVariable Long id, @RequestBody Produto obj) {
-		return obj = service.updateProdutoById(id, obj);
+	public ResponseEntity<Produto> updateProduto(@PathVariable Long id, @RequestBody Produto obj) {
+		 obj = service.updateProdutoById(id, obj);
+		 return ResponseEntity.ok(obj);
 	}
 }
--- src/main/java/com/example/sb/service/ProdutoService.java
+++ src/main/java/com/example/sb/service/ProdutoService.java
@@ -2,50 +2,64 @@ package com.example.sb.service;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.example.sb.entity.Produto;
+import com.example.sb.exceptions.DatabaseIntegrityException;
+import com.example.sb.exceptions.ResourceNotFoundException;
 import com.example.sb.repository.ProdutoRepository;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 
 @Service
-public class ProdutoService implements Serializable{
+public class ProdutoService implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Autowired
 	private ProdutoRepository repository;
 
-	public List<Produto> listarProdutos(){
+	public List<Produto> listarProdutos() {
 		return repository.findAll();
 	}
-	
-	public Optional<Produto> acharPorId(Long id) {
-		return repository.findById(id);
+
+	public Produto acharPorId(Long id) {
+		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public Produto adicionarProduto(Produto obj) {
 		return repository.save(obj);
 	}
-	
+
 	@Transactional
 	public void deleteById(Long id) {
-		 repository.deleteById(id);
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException(id);
+		}
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseIntegrityException("Produto não pôde ser deletado");
+		}
 	}
-	
+
 	public Produto updateProdutoById(Long id, Produto obj) {
-		Produto entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			Produto entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
-	
+
 	public void updateData(Produto entity, Produto obj) {
 		entity.setNome(obj.getNome());
 		entity.setPreco(obj.getPreco());
-		
+
 	}
 }
